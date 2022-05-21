486c902a3b39d44b0c33ad757eade5fa5bf2bd60
MustafaHi <matterik971@gmail.com>
2022-05-21 23:29:18 +0300

    +

--- script/main.js
+++ script/main.js
@@ -1,28 +1,21 @@
-const $ = document.querySelector;
+const $ = (s) => document.querySelector(s);
 const Desktop = $("#desktop").getContext("2d");
-const Mobile  = $("#mobile").getContext("2d");
+const Mobile  = $("#mobile") .getContext("2d");
 
-const Sets = [
-    {
-        background: "",
-        images: [],
-        text: [
-            {
-                x: 10, y: 20,
-                size: 20
-            },
-            {
-                x: 50, y: 50,
-                size: 12
-            }
-        ]
-    }
-]
-
-
-
-
-
+function paint(setIndex)
+{
+	const set = Sets[setIndex];
+	const BG  = new Image();
+				BG.src = set.background;
+	BG.onload = () => {
+		Desktop.drawImage(BG, 0, 0, 500, 500);
+		Desktop.fillText("first", set.text[0].x, set.text[0].y);
+		Desktop.fillText("second", set.text[1].x, set.text[1].y);
+	}
+}
+document.addEventListener("DOMContentLoaded", () => {
+	paint(0);
+});
 
 
 
@@ -36,3 +29,20 @@ function roundedRect(ctx, x, y, width, height, radius)
     ctx.arcTo(x, y, x, y + radius, radius);
     ctx.fill()
 }
+
+const Sets = [
+    {
+        background: "../asset/plus-bubble.png",
+        images: [],
+        text: [
+            {
+                x: 10, y: 20,
+                size: 20
+            },
+            {
+                x: 50, y: 50,
+                size: 12
+            }
+        ]
+    }
+]
